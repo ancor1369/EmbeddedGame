9ab01643597841c8fff000b70603857f14fab05e
Andres Cordoba <ancor1369@gmail.com>
2019-11-16 11:52:47 -0500

    Small modifications to the codebase

--- GraphicsManager/screenController/screenController.ino
+++ GraphicsManager/screenController/screenController.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <ArduinoJson.h>
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
