009a1071f81bfdd456be6c585f0b7333875a8d22
Andres Cordoba <ancor1369@gmail.com>
2019-11-16 00:27:25 -0500

    In this verison of the software I have added the manner to control the charcaters and have setted interesting ideas on how to manage the internals of the game. More work is needed to manage the communicaitons, however this work will be done

--- GraphicsManager/screenController/screenController.ino
+++ GraphicsManager/screenController/screenController.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "queue.h"
+#include <ArduinoJson.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -11,38 +11,42 @@
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     16 // Number of snowflakes in the animation example
+#define NUMFLAKES     5 // Number of snowflakes in the animation example
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
-static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+#define MISSILE_HEIGHT   8
+#define MISSILE_WIDTH    16
 
-void setup() {
-  Serial.begin(9600);
+String inputString = "";
+int x,y=0;
+int xt,yt=0;
+int xmi, ymi = 0;
+int scene ;
+int Bitmap;
 
+
+static const unsigned char PROGMEM logo_bmp[] = 
+{
+  0x00, 0x00, 0x04, 0x80, 0x0f, 0xc0, 0x0e, 0xe0, 0xcf, 0xf3, 0xc8, 0x03, 0xce, 0x03, 0xc8, 0x13, 
+  0x8f, 0xf3, 0x9f, 0xf9, 0xaf, 0xf5, 0x9e, 0x79, 0xfe, 0x7f, 0x0f, 0xf0, 0x02, 0x20, 0x02, 0x20
+};
+
+static const unsigned char PROGMEM missile_bmp[] =
+{
+  0xff, 0xf0, 0xfb, 0xa8, 0xf9, 0x54, 0xfc, 0x2a, 0xf9, 0x54, 0xfb, 0xa8, 0xff, 0xf0, 0x00, 0x00
+};
+
+void setup() { 
+  
+  Serial.begin(115200);
+   inputString.reserve(300);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) 
   { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
-  }
-
+  }    
   testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
 }
 
@@ -55,43 +59,79 @@ void loop()
 #define YPOS   1
 #define DELTAY 2
 
+void serialEvent();
+
 void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
   int8_t f, icons[NUMFLAKES][3];
 
-  // Initialize 'snowflake' positions
+ 
+
+for(;;) { // Loop forever...
+  display.clearDisplay(); // Clear the display buffer
+  serialEvent();
+  // Draw each snowflake:
   for(f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-    icons[f][YPOS]   = -LOGO_HEIGHT;
-    icons[f][DELTAY] = random(1, 6);
-    Serial.print(F("x: "));
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(F(" y: "));
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(F(" dy: "));
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  for(;;) { // Loop forever...
-    display.clearDisplay(); // Clear the display buffer
-
-    // Draw each snowflake:
-    for(f=0; f< NUMFLAKES; f++) {
+    if(f==1)
+    {
       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
     }
+    if(f==2)
+    {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], missile_bmp, MISSILE_WIDTH, MISSILE_HEIGHT, SSD1306_WHITE);
+    }
+  }
 
-    display.display(); // Show the display buffer on the screen
-    delay(200);        // Pause for 1/10 second
-    Serial.println("Data");
-    // Then update coordinates of each flake...
-    for(f=0; f< NUMFLAKES; f++) {
-      icons[f][YPOS] += icons[f][DELTAY];
-      // If snowflake is off the bottom of the screen...
-      if (icons[f][YPOS] >= display.height()) {
-        // Reinitialize to a random position, just off the top
-        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-        icons[f][YPOS]   = -LOGO_HEIGHT;
-        icons[f][DELTAY] = random(1, 6);
+  display.display(); // Show the display buffer on the screen
+  delay(15);        // Pause for 1/10 second    
+  // Then update coordinates of each flake...
+  for(f=0; f< NUMFLAKES; f++) {
+      
+      if(f==1)
+      {
+        icons[f][XPOS]   = x;
+        icons[f][YPOS]   = y;    
       }
+      if(f==2)
+      {
+        icons[f][XPOS]   = xmi;
+        icons[f][YPOS]   = ymi;       
+      }
+    }
+  }
+}
+
+
+
+
+void serialEvent() {  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();    
+    // add it to the inputString:
+    if(inChar != 'Z')
+    {
+        inputString += inChar; 
+    }
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == 'Z') 
+    {
+      Serial.println(inputString);            
+      sscanf(inputString.c_str(),"%d,%d,%d,%d",&scene,&Bitmap,&xt,&yt);
+      //This needs to go into the queue and then we can make this program to work as expected
+      //This is just a test to mive the characters as desired
+      if(Bitmap==1)
+      {
+        x=xt;
+        y=yt;
+      }
+      if(Bitmap==2)
+      {
+        xmi=xt;
+        ymi=yt;
+      }
+      inputString = "";
+      
     }
   }
 }
