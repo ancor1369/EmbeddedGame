c3f2bd8b1edc5721b6c94a821e7fd62987f738e4
Andres Cordoba <ancor1369@gmail.com>
2019-11-17 15:02:54 -0500

    Add git ignore to stop tracking unnecessary files and binaries

--- GameEngine/lpc_chip_175x_6x/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/andres/Documents/EMBT/Term3/Ese3025/EmbeddedGame/GameEngine/lpc_chip_175x_6x/inc/ring_buffer.h \
- /home/andres/Documents/EMBT/Term3/Ese3025/EmbeddedGame/GameEngine/lpc_chip_175x_6x/inc/lpc_types.h
-
-/home/andres/Documents/EMBT/Term3/Ese3025/EmbeddedGame/GameEngine/lpc_chip_175x_6x/inc/ring_buffer.h:
-
-/home/andres/Documents/EMBT/Term3/Ese3025/EmbeddedGame/GameEngine/lpc_chip_175x_6x/inc/lpc_types.h:
